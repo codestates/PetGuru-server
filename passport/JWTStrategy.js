6485da1ea6e0d63fc706f9316667a08ee1a95f3c
Your Name <junapossible@gmail.com>
2021-06-09 01:53:30 +0900

    :beers: 미싱 API 전체 구조 세팅 및 폴더 정리

---
+++ passport/JWTStrategy.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+const { Users } = require("../models");
+
+module.exports = () => {
+  const jwtStrategyOption = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+  };
+
+  const jwtVerify = (jwtPayload, done) => {
+    return Users.findOne({ where: { id: jwtPayload.id } })
+      .then((user) => {
+        return done(null, user);
+      })
+      .catch((err) => {
+        return done(err);
+      });
+  };
+
+  passport.use(new JWTStrategy(jwtStrategyOption, jwtVerify));
+};
---
+++ routes/missing.js
@@ -0,0 +1,17 @@
+const router = express.Router();
+const missingController = require('../controllers/missingController');
+const passport = require("passport");
+const { upload } = require("./multer"); //multer 구현해야 함
+
+router.post(
+    '/posts', 
+    passport.authenticate('jwt', { session: false }),
+    upload.array('img'),
+    missingController.register,
+);
+
+router.get('/posts', missingController.info);
+router.put('/posts', missingController.edit);
+router.delete('/posts', missingController.delete);
+
+module.exports = router;
