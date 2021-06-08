c643ab956ad34248102421bd2235593fb7f64541
Your Name <junapossible@gmail.com>
2021-06-08 22:17:45 +0900

    :art: 폴더구조 정리O auth 일부 구현

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const https =  require("https");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 const dotenv = require("dotenv");
 // const config = require('./config')
 const passport = require("passport");
@@ -9,10 +10,13 @@ const passportConfig = require("./passport");
 const session = require("express-session");
 
 const userRouter = require('./routes/users');
-const db = require("./models/");
+const petRouter = require('./routes/pet')
+const Router = require('./routes/pet')
+
+
+
 const express = require("express");
-const { param } = require("./routes/users");
-dotenv.config(); //dotenv 사용?
+dotenv.config(); 
 
 const app = express();
 
@@ -20,8 +24,8 @@ const app = express();
 passportConfig(); // 패스포트 설정
 app.use(express.json()); //front에서 json 형식의 데이터를 보냈을 때 데이터 req.body에 넣어준당
 app.use(express.urlencoded({ extended : true})); // form submit 했을 때 데이터를 req.body에 넣어준당
-
 app.use(cookieParser(process.env.COOKIE_SECRET));
+
 app.use(session({
   resave: false,
   saveUninitialized: true,
@@ -45,16 +49,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/user', userRouter)
-
-app.get('/auth/google',
-  // passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+app.use('/pet', petRouter)
+app.use('/missing', missingRouter)
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,20 @@ const router = express.Router();
 const userController = require('../controllers/userController')
 
 router.post('/signup', userController.signup);
-console.log('여기까지')
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
+
+
+//auth
+router.get('/auth/google',
+  // passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
 
 module.exports = router;
