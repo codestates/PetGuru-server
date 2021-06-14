d73d25e1169152129b0b52b8079957badb54c66d
Seul An Kim <sakim5027@gmail.com>
2021-06-14 01:11:12 -0500

    :art: migration setting modified
    migration setting modified

--- migrations/20210610031639-create-pet.js
+++ migrations/20210610031639-create-pet.js
@@ -28,11 +28,11 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.NOW
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.NOW
+        type: Sequelize.DATE
       }
     });
   },
--- migrations/20210610033008-create-question.js
+++ migrations/20210610033008-create-question.js
@@ -37,11 +37,11 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.NOW
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.NOW
+        type: Sequelize.DATE
       }
     });
   },
